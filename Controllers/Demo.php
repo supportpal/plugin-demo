f233041cd93108972bfc64dff04207406c72c67d
Jay <602425+jshah4517@users.noreply.github.com>
2024-12-06 15:17:13 +0000

    chore: fix demo notice position

--- Controllers/Demo.php
+++ Controllers/Demo.php
@@ -98,7 +98,7 @@ class Demo extends Plugin
     protected function setDemoNotice()
     {
         $notice = <<<END
-<div id="sp-demo-notice" class="sp-fixed sp-bottom-0 sp-left-0 sp-z-10001 sp-w-full sp-px-4 sp-py-2 sp-text-white sp-text-center" style="background: rgba(0, 0, 0, 0.8)">
+<div id="sp-demo-notice" class="sp-fixed sp-bottom-0 sp-start-0 sp-z-10001 sp-w-full sp-px-4 sp-py-2 sp-text-white sp-text-center" style="background: rgba(0, 0, 0, 0.8)">
     This is the SupportPal <a class="sp-text-white hover:sp-text-white" style="text-decoration: underline" href="https://www.supportpal.com/product/demo" target="_blank">product demo</a>. By
     continuing to navigate this website you agree to our 
     <a class="sp-text-white hover:sp-text-white" style="text-decoration: underline" href="https://www.supportpal.com/company/terms" target="_blank">Terms of Service</a>
