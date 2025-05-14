4823649ec7b0934711f9002f4b16dac5fc824d71
bytestream <kieran@supportpal.com>
2025-05-14 15:42:29 +0100

    update TicketCustomFieldSeeder.php

--- Seeds/Tickets/TicketCustomFieldSeeder.php
+++ Seeds/Tickets/TicketCustomFieldSeeder.php
@@ -3,6 +3,10 @@
 namespace Addons\Plugins\Demo\Seeds\Tickets;
 
 use App\Modules\Core\Controllers\Database\Seed\Seeder;
+use App\Modules\Core\Models\Brand;
+use App\Modules\Core\Models\CustomField;
+use App\Modules\Ticket\Models\Department;
+use App\Modules\User\Models\User;
 use DB;
 
 class TicketCustomFieldSeeder extends Seeder
@@ -14,96 +18,98 @@ class TicketCustomFieldSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('ticket_customfield')->insert([
-            [
-                'name'       => 'Installed Version',
-                'type'       => 5,
-                'order'      => 1,
-                'required'   => 1,
-                'public'     => 1,
-                'encrypted'  => 0,
-                'purge'      => 0,
-                'locked'     => 0,
-                'created_at' => time(),
-                'updated_at' => time()
-            ],
-            [
-                'name'       => 'Installation URL',
-                'type'       => 8,
-                'order'      => 2,
-                'required'   => 1,
-                'public'     => 1,
-                'encrypted'  => 0,
-                'purge'      => 0,
-                'locked'     => 0,
-                'created_at' => time(),
-                'updated_at' => time()
-            ],
-            [
-                'name'       => 'FTP Username',
-                'type'       => 8,
-                'order'      => 3,
-                'required'   => 0,
-                'public'     => 1,
-                'encrypted'  => 1,
-                'purge'      => 1,
-                'locked'     => 0,
-                'created_at' => time(),
-                'updated_at' => time()
-            ],
-            [
-                'name'       => 'FTP Password',
-                'type'       => 6,
-                'order'      => 4,
-                'required'   => 0,
-                'public'     => 1,
-                'encrypted'  => 1,
-                'purge'      => 1,
-                'locked'     => 0,
-                'created_at' => time(),
-                'updated_at' => time()
-            ],
-            [
-                'name'       => 'License Type',
-                'type'       => 7,
-                'order'      => 5,
-                'required'   => 0,
-                'public'     => 0,
-                'encrypted'  => 0,
-                'purge'      => 0,
-                'locked'     => 0,
-                'created_at' => time(),
-                'updated_at' => time()
-            ]
+        $field1 = DB::table('ticket_customfield')->insertGetId([
+            'name'       => 'Installed Version',
+            'type'       => CustomField::OPTIONS,
+            'order'      => 1,
+            'required'   => 1,
+            'public'     => 1,
+            'encrypted'  => 0,
+            'purge'      => 0,
+            'locked'     => 0,
+            'created_at' => time(),
+            'updated_at' => time()
+        ]);
+
+        $field2 = DB::table('ticket_customfield')->insertGetId([
+            'name'       => 'Installation URL',
+            'type'       => CustomField::TEXT,
+            'order'      => 2,
+            'required'   => 1,
+            'public'     => 1,
+            'encrypted'  => 0,
+            'purge'      => 0,
+            'locked'     => 0,
+            'created_at' => time(),
+            'updated_at' => time()
+        ]);
+
+        $field3 = DB::table('ticket_customfield')->insertGetId([
+            'name'       => 'FTP Username',
+            'type'       => CustomField::TEXT,
+            'order'      => 3,
+            'required'   => 0,
+            'public'     => 1,
+            'encrypted'  => 1,
+            'purge'      => 1,
+            'locked'     => 0,
+            'created_at' => time(),
+            'updated_at' => time()
+        ]);
+
+        $field4 = DB::table('ticket_customfield')->insertGetId([
+            'name'       => 'FTP Password',
+            'type'       => CustomField::PASSWORD,
+            'order'      => 4,
+            'required'   => 0,
+            'public'     => 1,
+            'encrypted'  => 1,
+            'purge'      => 1,
+            'locked'     => 0,
+            'created_at' => time(),
+            'updated_at' => time()
+        ]);
+
+        $field5 = DB::table('ticket_customfield')->insertGetId([
+            'name'       => 'License Type',
+            'type'       => CustomField::RADIO,
+            'order'      => 5,
+            'required'   => 0,
+            'public'     => 0,
+            'encrypted'  => 0,
+            'purge'      => 0,
+            'locked'     => 0,
+            'created_at' => time(),
+            'updated_at' => time()
         ]);
 
         DB::table('ticket_customfield_option')->insert([
             [
-                'field_id'   => 1,
+                'field_id'   => $field1,
                 'value'      => '2.0.0',
                 'created_at' => time(),
                 'updated_at' => time()
             ],
             [
-                'field_id'   => 1,
+                'field_id'   => $field1,
                 'value'      => '2.0.1',
                 'created_at' => time(),
                 'updated_at' => time()
             ],
             [
-                'field_id'   => 1,
+                'field_id'   => $field1,
                 'value'      => '2.0.2',
                 'created_at' => time(),
                 'updated_at' => time()
             ],
             [
-                'field_id'   => 5,
+                'field_id'   => $field5,
                 'value'      => 'Monthly',
                 'created_at' => time(),
                 'updated_at' => time()
             ],
             [
-                'field_id'   => 5,
+                'field_id'   => $field5,
                 'value'      => 'Owned',
                 'created_at' => time(),
                 'updated_at' => time()
@@ -111,37 +117,42 @@ class TicketCustomFieldSeeder extends Seeder
         ]);
 
         // Assign custom fields to brands.
+        $brand = Brand::where('name', 'SupportPal')->firstOrFail();
         DB::table('ticket_customfield_brand_membership')->insert([
-            [ 'field_id' => 1, 'brand_id' => 1 ],
-            [ 'field_id' => 2, 'brand_id' => 1 ],
-            [ 'field_id' => 3, 'brand_id' => 1 ],
-            [ 'field_id' => 4, 'brand_id' => 1 ],
-            [ 'field_id' => 5, 'brand_id' => 1 ],
+            [ 'field_id' => $field1, 'brand_id' => $brand->id ],
+            [ 'field_id' => $field2, 'brand_id' => $brand->id ],
+            [ 'field_id' => $field3, 'brand_id' => $brand->id ],
+            [ 'field_id' => $field4, 'brand_id' => $brand->id ],
+            [ 'field_id' => $field5, 'brand_id' => $brand->id ],
         ]);
 
         // Assign custom fields to departments
+        $support = Department::where('name', 'Support')->firstOrFail();
+        $sales = Department::where('name', 'Sales')->firstOrFail();
+        $bugs = Department::where('name', 'Bugs')->firstOrFail();
         DB::table('department_customfield_membership')->insert([
-            [ 'field_id' => 1, 'department_id' => 1 ],
-            [ 'field_id' => 1, 'department_id' => 4 ],
-            [ 'field_id' => 2, 'department_id' => 1 ],
-            [ 'field_id' => 2, 'department_id' => 4 ],
-            [ 'field_id' => 3, 'department_id' => 1 ],
-            [ 'field_id' => 3, 'department_id' => 4 ],
-            [ 'field_id' => 4, 'department_id' => 1 ],
-            [ 'field_id' => 4, 'department_id' => 4 ],
-            [ 'field_id' => 5, 'department_id' => 2 ],
+            [ 'field_id' => $field1, 'department_id' => $support->id ],
+            [ 'field_id' => $field1, 'department_id' => $bugs->id ],
+            [ 'field_id' => $field2, 'department_id' => $support->id ],
+            [ 'field_id' => $field2, 'department_id' => $bugs->id ],
+            [ 'field_id' => $field3, 'department_id' => $support->id ],
+            [ 'field_id' => $field3, 'department_id' => $bugs->id ],
+            [ 'field_id' => $field4, 'department_id' => $support->id ],
+            [ 'field_id' => $field4, 'department_id' => $bugs->id ],
+            [ 'field_id' => $field5, 'department_id' => $sales->id ],
         ]);
         
         // Add to activity log
+        $operator = User::operator()->firstOrFail();
         DB::table('activity_log')->insert([
             [
                 'type'          => 1,
                 'rel_name'      => 'Installed Version',
-                'rel_id'        => 1,
+                'rel_id'        => $field1,
                 'rel_route'     => 'ticket.operator.customfield.edit',
                 'section'       => 'ticket.customfield',
-                'user_id'       => 1,
-                'user_name'     => 'John Doe',
+                'user_id'       => $operator->id,
+                'user_name'     => $operator->formatted_name,
                 'event_name'    => 'item_created',
                 'ip'            => inet_pton('81.8.12.192'),
                 'created_at'    => time(),
@@ -150,11 +161,11 @@ class TicketCustomFieldSeeder extends Seeder
             [
                 'type'          => 1,
                 'rel_name'      => 'Installation URL',
-                'rel_id'        => 2,
+                'rel_id'        => $field2,
                 'rel_route'     => 'ticket.operator.customfield.edit',
                 'section'       => 'ticket.customfield',
-                'user_id'       => 1,
-                'user_name'     => 'John Doe',
+                'user_id'       => $operator->id,
+                'user_name'     => $operator->formatted_name,
                 'event_name'    => 'item_created',
                 'ip'            => inet_pton('81.8.12.192'),
                 'created_at'    => time(),
@@ -163,11 +174,11 @@ class TicketCustomFieldSeeder extends Seeder
             [
                 'type'          => 1,
                 'rel_name'      => 'FTP Username',
-                'rel_id'        => 3,
+                'rel_id'        => $field3,
                 'rel_route'     => 'ticket.operator.customfield.edit',
                 'section'       => 'ticket.customfield',
-                'user_id'       => 1,
-                'user_name'     => 'John Doe',
+                'user_id'       => $operator->id,
+                'user_name'     => $operator->formatted_name,
                 'event_name'    => 'item_created',
                 'ip'            => inet_pton('81.8.12.192'),
                 'created_at'    => time(),
@@ -176,11 +187,11 @@ class TicketCustomFieldSeeder extends Seeder
             [
                 'type'          => 1,
                 'rel_name'      => 'FTP Password',
-                'rel_id'        => 4,
+                'rel_id'        => $field4,
                 'rel_route'     => 'ticket.operator.customfield.edit',
                 'section'       => 'ticket.customfield',
-                'user_id'       => 1,
-                'user_name'     => 'John Doe',
+                'user_id'       => $operator->id,
+                'user_name'     => $operator->formatted_name,
                 'event_name'    => 'item_created',
                 'ip'            => inet_pton('81.8.12.192'),
                 'created_at'    => time(),
@@ -189,11 +200,11 @@ class TicketCustomFieldSeeder extends Seeder
             [
                 'type'          => 1,
                 'rel_name'      => 'License Type',
-                'rel_id'        => 5,
+                'rel_id'        => $field5,
                 'rel_route'     => 'ticket.operator.customfield.edit',
                 'section'       => 'ticket.customfield',
-                'user_id'       => 1,
-                'user_name'     => 'John Doe',
+                'user_id'       => $operator->id,
+                'user_name'     => $operator->formatted_name,
                 'event_name'    => 'item_created',
                 'ip'            => inet_pton('81.8.12.192'),
                 'created_at'    => time(),
