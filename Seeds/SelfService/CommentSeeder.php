e3c29629b4ec6a2f6d3f0065e8656ba34f64eca5
bytestream <kieran@supportpal.com>
2025-05-14 14:29:44 +0100

    remove hardcoded ids from CommentSeeder

--- Seeds/SelfService/CommentSeeder.php
+++ Seeds/SelfService/CommentSeeder.php
@@ -3,6 +3,10 @@
 namespace Addons\Plugins\Demo\Seeds\SelfService;
 
 use App\Modules\Core\Controllers\Database\Seed\Seeder;
+use App\Modules\Core\Models\Brand;
+use App\Modules\Selfservice\Models\Article;
+use App\Modules\Selfservice\Models\Type;
+use App\Modules\User\Models\User;
 use DB;
 
 class CommentSeeder extends Seeder
@@ -14,59 +18,65 @@ class CommentSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('comment')->insert([
-            [
-                'article_id'    => 6,
-                'type_id'       => 1,
-                'author_id'     => 3,
-                'text'          => $text = 'Looking forward to it!',
-                'purified_text' => $text,
-                'parent_id'     => null,
-                'root_parent_id'=> null,
-                'rating'        => 1,
-                'status'        => 1,
-                'notify_reply'  => 0,
-                'created_at'    => time(),
-                'updated_at'    => time()
-            ],
-            [
-                'article_id'    => 6,
-                'type_id'       => 1,
-                'author_id'     => 1,
-                'text'          => $text = '<a href=\'#comment-1\'>@PatrickMason</a> Thanks for your support!',
-                'purified_text' => $text,
-                'parent_id'     => 1,
-                'root_parent_id'=> 1,
-                'rating'        => 0,
-                'status'        => 1,
-                'notify_reply'  => 0,
-                'created_at'    => time(),
-                'updated_at'    => time()
-            ]
+        $announcements = Type::where('slug', 'announcements')->firstOrFail();
+        $article = Article::where('slug', 'welcome-to-the-supportpal-demo')->firstOrFail();
+
+        $brandId = Brand::where('name', 'SupportPal')->firstOrFail()->id;
+        $user = User::where('email', 'user@demo.com')->where('brand_id', $brandId)->firstOrFail();
+
+        $comment1 = DB::table('comment')->insertGetId([
+            'article_id'    => $article->id,
+            'type_id'       => $announcements->id,
+            'author_id'     => $user->id,
+            'text'          => $text = 'Looking forward to it!',
+            'purified_text' => $text,
+            'parent_id'     => null,
+            'root_parent_id'=> null,
+            'rating'        => 1,
+            'status'        => 1,
+            'notify_reply'  => 0,
+            'created_at'    => time(),
+            'updated_at'    => time()
+        ]);
+
+        $operator = User::operator()->firstOrFail();
+        $comment2 = DB::table('comment')->insertGetId([
+            'article_id'    => $article->id,
+            'type_id'       => $announcements->id,
+            'author_id'     => $operator->id,
+            'text'          => $text = '<a href=\'#comment-1\'>@PatrickMason</a> Thanks for your support!',
+            'purified_text' => $text,
+            'parent_id'     => 1,
+            'root_parent_id'=> 1,
+            'rating'        => 0,
+            'status'        => 1,
+            'notify_reply'  => 0,
+            'created_at'    => time(),
+            'updated_at'    => time()
         ]);
 
         DB::table('activity_log')->insert([
             [
-                'type'      => 2,
-                'rel_name'  => 'comment',
-                'rel_id'    => 1,
+                'type'      => \App\Modules\Core\Models\ActivityLog\Type::User->value,
+                'rel_name'  => 'selfservice.comment',
+                'rel_id'    => $comment1,
                 'rel_route' => 'selfservice.operator.comment.edit',
                 'section'   => 'selfservice.comment',
-                'user_id'   => 3,
-                'user_name' => 'Patrick Mason',
+                'user_id'   => $user->id,
+                'user_name' => $user->formatted_name,
                 'event_name'=> 'selfservice_comment_posted',
                 'ip'        => inet_pton('29.123.32.94'),
                 'created_at'=> time(),
                 'updated_at'=> time()
             ],
             [
-                'type'      => 1,
-                'rel_name'  => 2,
-                'rel_id'    => 2,
+                'type'      => \App\Modules\Core\Models\ActivityLog\Type::Operator->value,
+                'rel_name'  => 'selfservice.comment',
+                'rel_id'    => $comment2,
                 'rel_route' => 'selfservice.operator.comment.edit',
                 'section'   => 'selfservice.comment',
-                'user_id'   => 1,
-                'user_name' => 'John Doe',
+                'user_id'   => $operator->id,
+                'user_name' => $operator->formatted_name,
                 'event_name'=> 'item_created',
                 'ip'        => inet_pton('81.8.12.192'),
                 'created_at'=> time(),
