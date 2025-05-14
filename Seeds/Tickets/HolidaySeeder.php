1eb0f009f8f958cb93485966c28ea11d434486c2
bytestream <kieran@supportpal.com>
2025-05-14 15:29:48 +0100

    update HolidaySeeder

--- Seeds/Tickets/HolidaySeeder.php
+++ Seeds/Tickets/HolidaySeeder.php
@@ -3,6 +3,8 @@
 namespace Addons\Plugins\Demo\Seeds\Tickets;
 
 use App\Modules\Core\Controllers\Database\Seed\Seeder;
+use App\Modules\Core\Models\ActivityLog\Type;
+use App\Modules\User\Models\User;
 use DB;
 
 class HolidaySeeder extends Seeder
@@ -14,50 +16,50 @@ class HolidaySeeder extends Seeder
      */
     public function run()
     {
-        DB::table('schedule_holiday')->insert([
-            [
-                'name'       => 'Christmas Day',
-                'day'        => 25,
-                'month'      => 12,
-                'created_at' => time(),
-                'updated_at' => time()
-            ],
-            [
-                'name'       => 'New Years Day',
-                'day'        => 1,
-                'month'      => 1,
-                'created_at' => time(),
-                'updated_at' => time()
-            ],
+        $holiday1 = DB::table('schedule_holiday')->insertGetId([
+            'name'       => 'Christmas Day',
+            'day'        => 25,
+            'month'      => 12,
+            'created_at' => time(),
+            'updated_at' => time(),
+        ]);
+
+        $holiday2 = DB::table('schedule_holiday')->insertGetId([
+            'name'       => 'New Years Day',
+            'day'        => 1,
+            'month'      => 1,
+            'created_at' => time(),
+            'updated_at' => time(),
         ]);
 
         // Add to activity log
+        $operator = User::operator()->firstOrFail();
         DB::table('activity_log')->insert([
             [
-                'type'          => 1,
+                'type'          => Type::Operator->value,
                 'rel_name'      => 'Christmas Day',
-                'rel_id'        => 1,
+                'rel_id'        => $holiday1,
                 'rel_route'     => 'ticket.operator.holiday.edit',
                 'section'       => 'ticket.holiday',
-                'user_id'       => 1,
-                'user_name'     => 'John Doe',
+                'user_id'       => $operator->id,
+                'user_name'     => $operator->formatted_name,
                 'event_name'    => 'item_created',
                 'ip'            => inet_pton('81.8.12.192'),
                 'created_at'    => time(),
-                'updated_at'    => time()
+                'updated_at'    => time(),
             ],
             [
-                'type'          => 1,
+                'type'          => Type::Operator->value,
                 'rel_name'      => 'New Years Day',
-                'rel_id'        => 2,
+                'rel_id'        => $holiday2,
                 'rel_route'     => 'ticket.operator.holiday.edit',
                 'section'       => 'ticket.holiday',
-                'user_id'       => 1,
-                'user_name'     => 'John Doe',
+                'user_id'       => $operator->id,
+                'user_name'     => $operator->formatted_name,
                 'event_name'    => 'item_created',
                 'ip'            => inet_pton('81.8.12.192'),
                 'created_at'    => time(),
-                'updated_at'    => time()
+                'updated_at'    => time(),
             ],
         ]);
     }
